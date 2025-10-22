4905a6eaf4e881b344b44791d1ecd6aa18674ded
Viktor Elofsson <viktor@viktorelofsson.se>
2025-10-22 07:43:21 +0200

    wip

--- src/routes/_layout.index.tsx
+++ src/routes/_layout.index.tsx
@@ -1,12 +1,49 @@
+import Button from '@/components/button';
+import AddTorrentModal from '@/components/modals/add-torrent';
+import { useRPC } from '@/jsonrpc';
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react';
+
+type TorrentSearch = {
+  state?: "downloading" | "finished" | "seeding" | "paused" | "error"
+};
 
 export const Route = createFileRoute('/_layout/')({
   component: Index,
+  validateSearch: (search: Record<string, unknown>): TorrentSearch => {
+    return {
+      state: search.state === "downloading"
+        ? "downloading"
+        : undefined
+    }
+  }
 })
 
+type TorrentsList = {
+
+};
+
 function Index() {
+  const [addOpen, setAddOpen] = useState(false);
+
+  const torrents = useRPC<TorrentsList>("torrents.list", null, {
+    refreshInterval: 1000
+  });
+
   return (
-    <div className="p-2">
+    <div>
+      <AddTorrentModal
+        open={addOpen}
+        onClose={() => setAddOpen(false)}
+      />
+      <div className="flex items-center justify-between p-3 bg-gray-800">
+        <h1>torrents</h1>
+
+        <Button
+          text="Add torrent"
+          onClick={() => setAddOpen(true)}
+        />
+      </div>
       <h3>Welcome Home!</h3>
     </div>
   )
--- src/routes/_layout.tsx
+++ src/routes/_layout.tsx
@@ -19,7 +19,7 @@ type SysStatus = {
 }
 
 function RouteComponent() {
-  const [ status, setStatus ] = useState<SysStatus | undefined>();
+  const [status, setStatus] = useState<SysStatus | undefined>();
 
   useEffect(() => {
     fetch(prefixPath("/api/v1/system"))
@@ -45,21 +45,94 @@ function App() {
     return <Navigate to="/login" />
   }
 
+  if (!versions.data) {
+    return <div>loading</div>
+  }
+
+  return <AuthApp versions={versions.data} />
+}
+
+type AuthAppProps = {
+  versions: SysVersions;
+}
+
+type TorrentsOverviewSession = {
+  torrents_per_category: Record<string, number>;
+  torrents_per_state: Record<string, number>;
+  torrents_per_tag: Record<string, number>;
+  torrents_per_tracker: Record<string, number>;
+  torrents_total: number;
+}
+
+type TorrentsOverview = {
+  sessions: Record<string, TorrentsOverviewSession>;
+}
+
+function AuthApp({ versions }: AuthAppProps) {
+  const overview = useRPC<TorrentsOverview>("torrents.overview", null, {
+    refreshInterval: 1000
+  });
+
+  const torrentsAll = (to: TorrentsOverview) =>
+    Object.keys(to.sessions)
+      .map(k => to.sessions[k].torrents_total)
+      .reduce((prev, curr) => prev + curr);
+
+  if (!overview.data) {
+    return <div>loading</div>
+  }
+
   return (
     <div className="text-white h-dvh grid grid-cols-[300px_1fr]">
       <div className="bg-gray-700 border-r border-r-gray-500 h-dvh flex flex-col shadow-md">
         <div className="m-2 space-x-2">
           <Isotyope className="w-8" />
-          {versions.data?.boost.version}
+          {versions.boost.version}
           <div className="mt-3">
             <input type="text" placeholder="Search torrents..." className="w-full border border-gray-500 p-2 rounded bg-gray-600" />
           </div>
         </div>
         <div className="flex-1">
-          menu
+          <ul>
+            <li>
+              Torrents
+              <ul>
+                <li>
+                  <Link to="/" search={{}} activeOptions={{ exact: true }} activeProps={{ className: "font-bold" }}>
+                    All {torrentsAll(overview.data)}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" search={{ state: "downloading" }} activeProps={{ className: "font-bold" }}>
+                    Downloading {torrentsAll(overview.data)}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" search={{ state: "finished" }} activeProps={{ className: "font-bold" }}>
+                    Finished {torrentsAll(overview.data)}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" search={{ state: "seeding" }} activeProps={{ className: "font-bold" }}>
+                    Seeding {torrentsAll(overview.data)}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" search={{ state: "paused" }} activeProps={{ className: "font-bold" }}>
+                    Paused {torrentsAll(overview.data)}
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" search={{ state: "error" }} activeProps={{ className: "font-bold" }}>
+                    Error {torrentsAll(overview.data)}
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
         <div className="p-2">
-          <Link to="/settings">Settings</Link>
+          <Link to="/settings" activeProps={{ className: "font-medium bg-gray-800 text-white" }}>Settings</Link>
         </div>
       </div>
 
