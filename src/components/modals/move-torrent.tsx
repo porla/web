263bafa765d565df3b27436da8b08789642f5e29
Viktor Elofsson <viktor@viktorelofsson.se>
2025-12-01 14:40:30 +0100

    remove unused component

--- src/components/modals/move-torrent.tsx
+++ src/components/modals/move-torrent.tsx
@@ -1,6 +1,5 @@
 import { Suspense } from 'react';
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 import Button from '@/components/button';
 import { useAppForm } from '@/hooks/form';
 import { useInvoker, type Torrent } from '@/jsonrpc';
