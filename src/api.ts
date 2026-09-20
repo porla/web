fa3cb8fa7d6bab8920d9a68f9b13e87b3c1f1795
Viktor Elofsson <viktor@viktorelofsson.se>
2026-09-20 10:29:25 +0200

    fix: show error from add call (#19)
    * shows error message (if any) from `torrents.add` call

--- src/api.ts
+++ src/api.ts
@@ -298,13 +298,15 @@ export type SysVersions = {
 };
 
 export class RpcError extends Error {
-  data: any;
   code: number;
+  message: string;
+  data: unknown;
 
-  constructor(code: number, message: string, data: any) {
+  constructor(code: number, message: string, data: unknown) {
     super(message);
     this.name = "RpcError";
     this.code = code;
+    this.message = message;
     this.data = data;
   }
 }
